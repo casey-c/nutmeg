e22eb3bbf9f8eae9f00150720902a44d6575572b
casey <casey-c@users.noreply.github.com>
2018-04-24 16:36:23 -0400

    /basic-drawing: Auto stash before merge of "feature/basic-drawing" and "origin/feature/basic-drawing"

--- constants.h
+++ constants.h
@@ -1,7 +1,16 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+// TODO: remove old/dead constants
+
 #define GRID_SPACING 16
+#define BORDER_RADIUS 10
+
+#define EMPTY_CUT_SIZE (4 * GRID_SPACING)
+#define STATEMENT_SIZE (2 * GRID_SPACING)
+
+#define Z_NORMAL 0
+#define Z_RAISED 10
 #define BORDER_RADIUS 10
 
 #endif // CONSTANTS_H
