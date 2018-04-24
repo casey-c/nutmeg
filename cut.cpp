c4cc7ed5efde0a08ecae6047fbab9413cbaab893
casey <casey-c@users.noreply.github.com>
2018-04-24 16:35:57 -0400

    Updated cut's paint event

--- cut.cpp
+++ cut.cpp
@@ -1,11 +1,24 @@
 #include "cut.h"
+#include "constants.h"
 
 Cut::Cut()
 {
+        //QPointF br(pt.x() + qreal(EMPTY_CUT_SIZE),
+                   //pt.y() + qreal(EMPTY_CUT_SIZE));
+        //drawBox = QRectF(pt, br);
 
 }
 
 
 void Cut::paint(QPainter* painter,
            const QStyleOptionGraphicsItem* option,
-                QWidget* widget) {}
+                QWidget* widget) {
+    if (mouseDown)
+      painter->setBrush(QBrush(gradClicked));
+    else if (highlighted)
+      painter->setBrush(QBrush(gradHighlighted));
+    else
+      painter->setBrush(QBrush(gradDefault));
+
+    painter->drawRoundedRect(drawBox, qreal(BORDER_RADIUS), qreal(BORDER_RADIUS));
+}
